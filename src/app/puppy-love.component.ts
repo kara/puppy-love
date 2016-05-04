4fcf1d172f9e1ea6c69a685199c0c07ed40765bf
Kara Erickson <karakara@google.com>
2016-05-04 14:53:19 -0700

    woof

--- src/app/puppy-love.component.ts
+++ src/app/puppy-love.component.ts
@@ -1,11 +1,54 @@
 import { Component } from '@angular/core';
+import {MdToolbar} from '@angular2-material/toolbar';
+import {MdButton} from '@angular2-material/button';
+import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav';
+import {MD_LIST_DIRECTIVES} from '@angular2-material/list';
+import {MD_CARD_DIRECTIVES} from '@angular2-material/card';
+import {MdInput} from '@angular2-material/input';
+import {MdCheckbox} from '@angular2-material/checkbox';
+import {MdRadioButton, MdRadioGroup, MdRadioDispatcher} from '@angular2-material/radio';
+import {MdIcon, MdIconRegistry} from '@angular2-material/icon';
 
 @Component({
   moduleId: module.id,
   selector: 'puppy-love-app',
   templateUrl: 'puppy-love.component.html',
-  styleUrls: ['puppy-love.component.css']
+  styleUrls: ['puppy-love.component.css'],
+  directives: [
+    MD_SIDENAV_DIRECTIVES,
+    MD_LIST_DIRECTIVES,
+    MD_CARD_DIRECTIVES,
+    MdToolbar,
+    MdButton,
+    MdInput,
+    MdCheckbox,
+    MdRadioGroup,
+    MdRadioButton,
+    MdIcon
+  ],
+  providers: [MdIconRegistry, MdRadioDispatcher],
+
 })
 export class PuppyLoveAppComponent {
-  title = 'puppy-love works!';
+  formShowing: boolean = false;
+  views: Object[] = [
+    {
+      name: "My Account",
+      description: "Edit my account information",
+      icon: "assignment ind"
+    },
+    {
+      name: "Potential dates",
+      description: "Find your soulmate!",
+      icon: "pets"
+    }
+  ];
+  dogs: Object[] = [
+    {name: "Porter"},
+    {name: "Mal"},
+    {name: "Razzle"},
+    {name: "Koby"},
+    {name: "Molly"},
+    {name: "Husi"}
+  ];
 }
